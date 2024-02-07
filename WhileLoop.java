d4521d27cabd34deac400995804e251467b05f66
iskaddy <159223792+iskaddy@users.noreply.github.com>
2024-02-07 15:59:47 +0800

    5th commit
    while loooooooooooooooooooooop

---
+++ WhileLoop.java
@@ -0,0 +1,13 @@
+public class ForLoopMOD {
+
+    public static void main(String[] args) {
+        
+        int i = 1;
+        
+        for (  ; i <= 10; i++ ){
+            System.out.println(i);
+        }
+    
+    }
+    
+}
