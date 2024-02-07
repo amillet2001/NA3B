e4cbaf4906d9d7228601f3cd48b76b59e0f56167
iskaddy <159223792+iskaddy@users.noreply.github.com>
2024-02-07 15:47:03 +0800

    1st commit
    Calculator program

---
+++ Calculator.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Calculator{
+public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+
+	System.out.print("Enter your first number: ");
+	double num1 = sc.nextDouble();
+	
+	System.out.print("Enter your second number: ");
+	double num2 = sc.nextDouble();
+
+
+}
+}
